ad760854c3f5eb1fcc7215dcdb81bc4cb289a03f
Paulina <paulinaa.staszynska@gmail.com>
2021-06-25 22:42:06 +0200

    Upload all files

---
+++ app.js
@@ -0,0 +1,15 @@
+const menuLinkClick = () => {
+    $('.menu__link').on('click', function () {
+
+        $('body, html').animate({
+            scrollTop: $('[data-section=' + $(this).attr('id') + ']').offset().top
+        }, 1000);
+
+    });
+}
+
+const app = () => {
+    menuLinkClick();
+}
+
+app();

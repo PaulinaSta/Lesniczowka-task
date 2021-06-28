2339a59a71acaa41b3388fa6a0cfed0220196929
mefioo <maciek1271@gmail.com>
2021-06-28 11:04:30 +0200

    Correcting media queries

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const menuLinkClick = () => {
-    $('.logo,.menu__link').on('click', function () {
+    $('.navbar__logo-wrapper,.menu__link').on('click', function () {
 
         $('body, html').animate({
             scrollTop: $('[data-section=' + $(this).attr('id') + ']').offset().top
         }, 1000);
 
     });
+}
+
+const app = () => {
+    menuLinkClick();
 }
 
 app();
